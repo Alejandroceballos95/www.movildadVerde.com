32da98f3cf5e943aced715f82df3844d1a74a977
Alejandro Ceballos <alejandro.ceballos95@gmail.com>
2024-11-28 20:45:11 -0500

    agregue servidos flask para mostrar la pagina web y organice los archivos con estrctura para servidor

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+#ruta para la pagina de inicio 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#ruta para datos comparativos
+@app.route('/')
+def datos_comparativos():
+    return render_template('datoscomparativos.html')
+
+#ruta para e-fuel
+@app.route('/')
+def e_fuel():
+    return render_template('e-fuel.html')
+
+#ruta para electricos
+@app.route('/')
+def electricos():
+    return render_template('electricos.html')
+
+#ruta para hibridos
+@app.route('/')
+def hibridos():
+    return render_template('hibridos.html')
+
+#ruta para archivos estaticos
+
+
